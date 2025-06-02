060f5e588e3c7cd5f8082d17fec7f9f94e3bff84
Richardy_Rodrigues <richardyrodrigues519@gmail.com>
2025-06-02 17:06:48 +0000

    Listando itens no home com ListView.builder

--- lib/Home.dart
+++ lib/Home.dart
@@ -8,13 +8,44 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List _listaViagens = ["Local 1", "Local 2", "Local 3"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(("Viagens")),
+        title: const Text("Viagens", style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blue,
       ),
-      body: Container(),
+      body: Column(children: <Widget>[
+        Expanded(
+            child: ListView.builder(
+                itemCount: _listaViagens.length,
+                itemBuilder: (context, index) {
+                  String titulo = _listaViagens[index];
+                  return GestureDetector(
+                    onTap: () {},
+                    child: Card(
+                        child: ListTile(
+                      title: Text(titulo),
+                      trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            GestureDetector(
+                              onTap: () {},
+                              child: const Padding(
+                                padding: EdgeInsets.all(8),
+                                child: Icon(
+                                  Icons.remove_circle,
+                                  color: Colors.red,
+                                ),
+                              ),
+                            )
+                          ]),
+                    )),
+                  );
+                }))
+      ]),
     );
   }
 }
