ff4393e070062be40dd64c25cc99befe1392ba9b
Richardy_Rodrigues <richardyrodrigues519@gmail.com>
2025-06-04 19:21:55 +0000

    Configurando o APP para salvar as localizações e mostrar ao usuários

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:minhas_viagens/SplashScreen.dart';
+import 'SplashScreen.dart';
 
-void main() => runApp(const MaterialApp(
-      title: "Minhas viagens",
-      home: SplashScreen(),
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Minhas Viagens",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+          centerTitle: true,
+        ),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
+      ),
+      home: const SplashScreen(),
       debugShowCheckedModeBanner: false,
-    ));
+    );
+  }
+}
